0a7bc8e8854df4ad9d94e03c52882b588b290bf8
Bhumika <bhumika@192.168.1.7>
2026-03-09 12:47:20 +0530

    useful git

--- test.php
+++ test.php
@@ -2,5 +2,8 @@
 //echo phpinfo();
 
 echo "Hello Git";
-echo "Git is important and interesting too and let's do more!"
+echo "<br>";
+echo "Git is important and interesting too and let's do more!";
+echo "<br>";
+echo "Git is very useful";
 ?>
